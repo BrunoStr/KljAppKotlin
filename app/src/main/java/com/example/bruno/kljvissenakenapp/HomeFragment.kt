8fc80fdff1355315754b0c7c80e25718b2a0a072
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-11-05 22:51:44 +0100

    Toolbar icon toegevoegd

--- app/src/main/java/com/example/bruno/kljvissenakenapp/HomeFragment.kt
+++ app/src/main/java/com/example/bruno/kljvissenakenapp/HomeFragment.kt
@@ -2,14 +2,14 @@ package com.example.bruno.kljvissenakenapp
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment:Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         activity?.setTitle(R.string.home_title)
+        setHasOptionsMenu(true)
         return inflater?.inflate(R.layout.fragment_home, null)
     }
 
@@ -18,5 +18,17 @@ class HomeFragment:Fragment(){
         super.onViewCreated(view, savedInstanceState)
 
         logoImg.setImageResource(R.drawable.logo2)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.fragment_home_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+
     }
 }
