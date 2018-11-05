975c874a92700cf9906e55a66faaf99c752784c9
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-11-05 22:27:01 +0100

    Start opmaak HomeFragment

--- app/src/main/java/com/example/bruno/kljvissenakenapp/HomeFragment.kt
+++ app/src/main/java/com/example/bruno/kljvissenakenapp/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment:Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,5 +16,7 @@ class HomeFragment:Fragment(){
     //Deze methode is het equivalent van de OnCreate() methode in een activity
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        logoImg.setImageResource(R.drawable.logo2)
     }
 }
