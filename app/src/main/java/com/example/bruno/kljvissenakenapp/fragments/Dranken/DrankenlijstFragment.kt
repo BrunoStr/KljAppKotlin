ca1640e4f488c29927655eb1f299ce6269f8d154
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-12-23 17:16:58 +0100

    Recyclerview Werkende

--- app/src/main/java/com/example/bruno/kljvissenakenapp/fragments/Dranken/DrankenlijstFragment.kt
+++ app/src/main/java/com/example/bruno/kljvissenakenapp/fragments/Dranken/DrankenlijstFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bruno.kljvissenakenapp.R
+import com.example.bruno.kljvissenakenapp.adapters.LidAdapter
+import com.example.bruno.kljvissenakenapp.ui.LidViewModel
+import kotlinx.android.synthetic.main.fragment_drankenlijst.*
 
 class DrankenlijstFragment: androidx.fragment.app.Fragment(){
+
+    lateinit var lidViewModel: LidViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         activity?.setTitle(R.string.drank_title)
         return inflater?.inflate(R.layout.fragment_drankenlijst, null)
@@ -15,5 +25,20 @@ class DrankenlijstFragment: androidx.fragment.app.Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val recycler = view!!.findViewById(R.id.ledenRecycler) as RecyclerView
+        recycler.layoutManager = LinearLayoutManager(activity!!.applicationContext)
+        recycler.setHasFixedSize(true)
+
+        val adapter = LidAdapter()
+        recycler.adapter = adapter
+
+        lidViewModel = ViewModelProviders.of(activity!!).get(LidViewModel::class.java)
+        lidViewModel.getAll().observe(this, Observer {leden ->
+
+            adapter.setLeden(leden)
+
+        })
+
     }
 }
