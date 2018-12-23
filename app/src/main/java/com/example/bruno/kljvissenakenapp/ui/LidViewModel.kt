41a318b18e0b98cafc2cc6737ee472aeee8d2898
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-12-23 12:42:19 +0100

    Aanmaken LidViewModel class

---
+++ app/src/main/java/com/example/bruno/kljvissenakenapp/ui/LidViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.bruno.kljvissenakenapp.ui
+
+import android.app.Application
+import androidx.annotation.NonNull
+import androidx.lifecycle.AndroidViewModel
+
+class LidViewModel(@NonNull application: Application): AndroidViewModel(application) {
+
+}
