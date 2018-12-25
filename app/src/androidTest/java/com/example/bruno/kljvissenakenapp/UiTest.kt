76842bec10a7c0e5629ef8140b0c908a1c505d33
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-12-25 17:23:04 +0100

    Item verwijderen test

--- app/src/androidTest/java/com/example/bruno/kljvissenakenapp/UiTest.kt
+++ app/src/androidTest/java/com/example/bruno/kljvissenakenapp/UiTest.kt
@@ -1,20 +1,25 @@
 package com.example.bruno.kljvissenakenapp
 
 import android.content.pm.ActivityInfo
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.example.bruno.kljvissenakenapp.activities.MainActivity
+import com.example.bruno.kljvissenakenapp.adapters.LidViewHolder
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+
+
 
 
 @RunWith(JUnit4::class)
@@ -62,5 +67,45 @@ class UiTest{
 
 
     }
+
+    @Test
+    fun deleteSelectedSchuld(){
+        InstrumentationRegistry.getTargetContext().deleteDatabase("Lid_database")
+        mainActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        onView(withId(R.id.main_menu_scrollview)).perform(ViewActions.swipeUp())
+        onView(withId(R.id.main_drankenBtn)).perform(click())
+
+        onView(withId(R.id.addLidBtn)).perform(click())
+        onView(withId(R.id.editNameTxt)).perform(typeText("Verwijder item test"))
+        onView(withId(R.id.editBedragTxt)).perform(typeText("12"))
+        onView(withId(R.id.editOmschrijvingTxt)).perform(typeText("Deze omschrijving dient om te testen"))
+        onView(withText("Opslaan")).perform(click())
+        onView(withId(R.id.ledenRecycler)).check(matches(hasDescendant(withText("Test Portret"))))
+
+
+        val recycler = mainActivityTestRule.activity.findViewById<RecyclerView>(R.id.ledenRecycler)
+        val itemCount = recycler.getAdapter()!!.getItemCount()
+
+        onView(withId(R.id.ledenRecycler))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<LidViewHolder>(itemCount-1, ViewActions.swipeLeft()));
+
+        onView(withId(R.id.ledenRecycler)).check(matches(hasChildCount(itemCount-1)))
+    }
+
+
+    @Test
+    fun deleteAllSchulden(){
+        mainActivityTestRule.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        onView(withId(R.id.main_menu_scrollview)).perform(ViewActions.swipeUp())
+        onView(withId(R.id.main_drankenBtn)).perform(click())
+
+        openActionBarOverflowOrOptionsMenu(mainActivityTestRule.activity.applicationContext)
+        onView(withText("Verwijder alle schulden")).perform(click())
+        onView(withId(R.id.ledenRecycler)).check(matches(hasChildCount(0)))
+
+
+
+    }
+
 
 }
