971043720a59d7519225d82cad1ac774f7d18511
Bruno Stroobants <bruno.stroobants.y9084@student.hogent.be>
2018-12-26 13:32:53 +0100

    Basislayout (Portret)

--- app/src/main/java/com/example/bruno/kljvissenakenapp/fragments/Contact/ContactFragment.kt
+++ app/src/main/java/com/example/bruno/kljvissenakenapp/fragments/Contact/ContactFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.bruno.kljvissenakenapp.R
+import kotlinx.android.synthetic.main.fragment_contact.*
 
 class ContactFragment: androidx.fragment.app.Fragment(){
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -15,5 +17,7 @@ class ContactFragment: androidx.fragment.app.Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fbLogo.setImageResource(R.drawable.fb_logo)
+        instaLogo.setImageResource(R.drawable.insta_logo)
     }
 }
